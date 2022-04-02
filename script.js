3e519b0d655ca3da5588a7f9497194123d1390f3
Gameishard <peterbray@internode.on.net>
2022-04-02 22:44:33 +1030

    actual files

---
+++ script.js
@@ -0,0 +1,12 @@
+// Automatic Slideshow - change image every 3 seconds
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
